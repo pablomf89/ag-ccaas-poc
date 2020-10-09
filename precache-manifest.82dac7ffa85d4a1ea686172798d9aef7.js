35d4773c96e92c0919b680ee291f85e23b178123
pablo m <pablomf89@gmail.com>
2020-10-09 12:44:38 +0200

    Updates

---
+++ precache-manifest.82dac7ffa85d4a1ea686172798d9aef7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce79f3d907a4544b021457ff4ca7337f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc7ed628ca9a931d5be2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45a7ef9d7a9e0685df69",
+    "url": "/static/js/2.d40c48bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d40c48bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7ed628ca9a931d5be2",
+    "url": "/static/js/main.9e65a9b7.chunk.js"
+  },
+  {
+    "revision": "33c59b1f0e3fd3bcf56a",
+    "url": "/static/js/runtime-main.8410f245.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
